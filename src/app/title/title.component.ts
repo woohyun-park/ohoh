502beb282d273fe32dfe15aea90efd94b752fec6
Woohyun Park <woohyunpark.mail@gmail.com>
2021-05-12 23:55:35 +0900

    ADD, update functions

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -5,14 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
+
 export class TitleComponent implements OnInit {
-  userName: string = "dooddi";
-  streak: number = 42;
-  goal: string = "매일매일 프로그래밍"
+  userName: string = "";
+  streak: number = 0;
+  goal: string = ""
 
   constructor() { }
 
   ngOnInit(): void {
+    if(localStorage.userName !== undefined)
+      this.userName = localStorage.userName;
+    if(localStorage.streak !== undefined)
+      this.streak = localStorage.streak;
+    if(localStorage.goal !== undefined)
+      this.goal = localStorage.goal;
   }
 
+  updateUserName(): void {
+    localStorage.userName = this.userName;
+  }
+
+  updateStreak(): void {
+    localStorage.streak = this.streak;
+  }
+
+  updateGoal(): void {
+    localStorage.goal = this.goal;
+  }
 }
